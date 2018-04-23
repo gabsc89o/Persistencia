c582ea64be9bf71bb2825c322c7bc0a86e61cae1
gabsc89o <user138066@eu249.local>
2018-04-23 17:48:11 +0100

    Initial Commit

---
+++ Persistencia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Persistencia
+//
+//  Created by user138066 on 4/23/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
